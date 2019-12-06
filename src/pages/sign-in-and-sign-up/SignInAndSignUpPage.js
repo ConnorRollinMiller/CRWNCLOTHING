48496e058a26a72671289c1dc0aae5807ce45423
ConnorMiller21 <miller.connor95@gmail.com>
2019-12-06 13:53:02 -0600

    Converted components to styled-components

--- src/pages/sign-in-and-sign-up/SignInAndSignUpPage.js
+++ src/pages/sign-in-and-sign-up/SignInAndSignUpPage.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import SignIn from '../../components/sign-in/SignIn';
 import SignUp from '../../components/sign-up/SignUp';
 
-import './SignInAndSignUpPage.scss';
-
-const SignInAndSignUp = () => {
-   return (
-      <div className='sign-in-and-sign-up'>
-         <SignIn />
-         <SignUp />
-      </div>
-   );
-};
+const SignInAndSignUpContainer = styled.main`
+   width: 850px;
+   display: flex;
+   justify-content: space-between;
+   margin: 30px auto;
+`;
+const SignInAndSignUp = () => (
+   <SignInAndSignUpContainer>
+      <SignIn />
+      <SignUp />
+   </SignInAndSignUpContainer>
+);
 
 export default SignInAndSignUp;

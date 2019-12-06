3aee2128d1231d155c772fd849cd24d62157a87e
ConnorMiller21 <miller.connor95@gmail.com>
2019-12-06 02:00:04 -0600

    Getting rid of redux-logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
 import { persistStore } from 'redux-persist';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 import rootReducer from './reducers';
 
 const middlewares = [];
-
-if (process.env.NODE_ENV === 'development') {
-   middlewares.push(logger);
-}
 
 export const store = createStore(
    rootReducer,
